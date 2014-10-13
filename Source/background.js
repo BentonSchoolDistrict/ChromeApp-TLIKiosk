5dbd91c6a333fcdee354e6ec64d0893e933e3740
Brian Lowrance <blowrancebenton@users.noreply.github.com>
2014-10-13 16:47:20 -0500

    Initial Commit to Dev - Debug, bug
    Initial Commit to Dev.
    Working on debug div bug; will not display more than once.
    Dev. Ver: 1.0.0.10

---
+++ Source/background.js
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////////////////////////
+// background.js
+// Handles background jobs for the app
+//
+// ChromeApp:  BPSD - TLI KIOSK
+// Author:  Brian S. Lowrance
+// Version: 1.0.0.9
+//
+/////////////////////////////////////////////////////////////////////
+
+var appWin = null;
+
+chrome.app.runtime.onLaunched.addListener(function() {
+	var options = {
+		'id': 'BPSD - TLI Kiosk',
+		'state': 'maximized',
+		'resizable': false,
+		'bounds': {
+			'width': 1024,
+			'height': 768
+		}
+	};
+	chrome.app.window.create('application.html', (options), onWindowCreated);
+});	
+
+function onWindowCreated(win) {
+	appWin = win
+	appWin.onBoundsChanged.addListener(function() {
+		//appWin.maximize();
+		appWin.fullscreen();
+	});
+};
